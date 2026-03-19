838b349491b9fe1750f7946a03c2b5b007c59d95
Paucom9 <pau.colom.montojo@gmail.com>
2026-03-19 08:22:22 +0100

    delete climate vars calculation

--- pheno_plasticity.R
+++ pheno_plasticity.R
@@ -29,50 +29,14 @@ library(Matrix)
 here::here() # Check the current working directory
 # ---
 phenology_estimates  <- read.csv(here::here("output", "pheno_estimates_allspp.csv"), sep = ",", dec = ".")
-mean_temperature <- read.csv(here::here("output", "climate", "mean_annual_temperature.csv"), sep = ",", dec = ".")
+clim_vars <- read.csv(here::here("output", "climate", "climate_variables.csv"), sep = ",", dec = ".")
 ebms_transect_coord <- read.csv(here::here("data", "ebms_transect_coord.csv"), sep = ",", dec = ".")
 species_traits <- read.csv(here::here("data", "species_trait_table.csv"), sep = ";", dec = ".")
 
 str(phenology_estimates)
-str(mean_temperature)
+str(clim_vars)
 str(ebms_transect_coord)
 str(species_traits)
-
-# ---
-
-#### Calculate within-temperature(1) and climate predictability(2) ####
-#(1) i.e., temporal anomaly around each transect’s long-term mean
-#(2) i.e., temporal autocorrelation
-
-clim_vars <- mean_temperature |>
-  arrange(transect_id, year) |>
-  group_by(transect_id) |>
-  mutate(
-    clim_background = mean(avg_temp, na.rm = TRUE),
-    clim_anomaly    = avg_temp - clim_background,
-    n_years         = sum(!is.na(avg_temp)),
-    
-    # Predictability based on variability (higher = more predictable)
-    clim_pred_sd = -sd(avg_temp, na.rm = TRUE),
-    
-    # Predictability based on temporal structure (lag-1 autocorrelation)
-    clim_pred_lag = if (first(n_years) >= 10) {
-      acf(avg_temp, plot = FALSE, lag.max = 1,
-          na.action = na.pass)$acf[2]
-    } else {
-      NA_real_
-    }
-  ) |>
-  ungroup() |>
-  mutate(
-    clim_background_sc = scale(clim_background)[,1],
-    clim_anomaly_sc    = scale(clim_anomaly)[,1],
-    clim_pred_sd_sc    = scale(clim_pred_sd)[,1],
-    clim_pred_lag_sc   = scale(clim_pred_lag)[,1]
-  )
-
-str(clim_vars)
-
 
 # ---
 
