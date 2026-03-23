05b94e0b7845cec1a4fc14c65dafdb40776b6ff2
Paucom9 <pau.colom.montojo@gmail.com>
2026-03-23 12:07:35 +0100

    minor

--- create_species_region_table.R
+++ create_species_region_table.R
@@ -1,10 +1,16 @@
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(here)
 
 #### Data ----
 phenology_estimates <- read.csv(here::here("output", "pheno_estimates_allspp.csv"))
 CBMS_traits         <- read.csv(here::here("data", "Traits_CBMS.csv"), sep = ";")
+species_traits <- read.csv(here::here("data", "species_trait_table.csv"), sep = ";", dec = ".")
+vol_se <- read.csv(here::here("data","voltinism", "species_country_voltinism_SE.csv"), sep = ";", dec = ".")
+vol_nl <- read.csv(here::here("data","voltinism", "species_country_voltinism_NL.csv"), sep = ";", dec = ".")
+vol_fi <- read.csv(here::here("data","voltinism", "species_country_voltinism_FI.csv"), sep = ";", dec = ".")
+
 
 #### Species × site → country ----
 df_species_site <- phenology_estimates %>%
@@ -61,6 +67,50 @@ species_country_volt <- species_country_volt %>%
     )
   ) %>%
   select(-voltinism_2, -vol_max)
+
+# Merge with SE, FI and NL
+
+vol_se <- vol_se %>%
+  select(SPECIES, SEBMS)
+vol_fi <- vol_fi %>%
+  select(SPECIES, FIBMS)
+vol_nl <- vol_nl %>%
+  select(SPECIES,  NLBMS)
+
+str(species_country_volt)
+str(vol_se)
+str(vol_fi)
+str(vol_nl)
+
+# Complete Sweeden
+species_country_volt <- species_country_volt %>%
+  left_join(vol_se, by = "SPECIES", suffix = c("", "_new")) %>%
+  mutate(
+    SEBMS = ifelse(SEBMS == "" | is.na(SEBMS), SEBMS_new, SEBMS)
+  ) %>%
+  select(-SEBMS_new)
+
+# Complete Finland
+species_country_volt <- species_country_volt %>%
+  left_join(vol_fi, by = "SPECIES", suffix = c("", "_new")) %>%
+  mutate(
+    FIBMS = ifelse(FIBMS == "" | is.na(FIBMS), FIBMS_new, FIBMS)
+  ) %>%
+  select(-FIBMS_new)
+
+# Complete Netherlands
+species_country_volt <- species_country_volt %>%
+  left_join(vol_nl, by = "SPECIES", suffix = c("", "_new")) %>%
+  mutate(
+    NLBMS = ifelse(NLBMS == "" | is.na(NLBMS), NLBMS_new, NLBMS)
+  ) %>%
+  select(-NLBMS_new)
+
+# Complete Luxembourgh
+species_country_volt <- species_country_volt %>%
+  mutate(
+    LUBMS = ifelse(LUBMS == "" | is.na(LUBMS), "multivoltine", LUBMS)
+  )
 
 #### Save ----
 write.csv(
